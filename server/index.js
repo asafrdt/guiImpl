9a72af433b8ecca3d9db4d56967092ede94c43a5
asafrdt <asafrdt@gmail.com>
2020-02-27 12:44:31 +0530

    Added Mongoose Connection

--- server/index.js
+++ server/index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const path = require('path');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
-
+const mongoose = require('mongoose');
 const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 5000;
+
+var Form = require('./models/form');
+
+mongoose.connect('mongodb://localhost/formbuilder');
 
 // Multi-process to utilize all CPU cores.
 if (!isDev && cluster.isMaster) {
@@ -27,16 +31,37 @@ if (!isDev && cluster.isMaster) {
 
   // Answer API requests.
   app.get('/api', function (req, res) {
+
+    var formName ="";
+    // create a new Form called Form1
+    var form = new Form({
+      name: 'Form1'
+    });
+
+    // call the custom method.
+    // forms will now be Form1
+    form.getFormName(function (err, name) {
+      if (err) throw err;
+      console.log('Form name is ' + name);
+      formName  = name;
+    });
+
+    // call the built-in save method to save to the database
+    form.save(function (err) {
+      if (err) throw err
+      console.log('Form saved successfully!');
+    });
+
     res.set('Content-Type', 'application/json');
-    res.send('{"message":"Hello from the custom server!"}');
+    res.send('{"message":"'+formName+'"}');
   });
 
   // All remaining requests return the React app, so it can handle routing.
-  app.get('*', function(request, response) {
+  app.get('*', function (request, response) {
     response.sendFile(path.resolve(__dirname, '../form-builder-ui/build', 'index.html'));
   });
 
   app.listen(PORT, function () {
-    console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
+    console.error(`Node ${isDev ? 'dev server' : 'cluster worker ' + process.pid}: listening on port ${PORT}`);
   });
 }
---
+++ server/models/form.js
@@ -0,0 +1,22 @@
+// grab the things we need
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create a schema
+var formSchema = new Schema({
+    name: String
+});
+
+formSchema.methods.getFormName = function () {
+    // add some stuff to the form name
+    this.name = this.name;
+
+    return this.name;
+};
+
+// the schema is useless so far
+// we need to create a model using it
+var Form = mongoose.model('Form', formSchema);
+
+// make this available to our forms in our Node applications
+module.exports = Form;
