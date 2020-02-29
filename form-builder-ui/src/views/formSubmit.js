62ffa52b156c8bbb6ac17284abc340ed1c8bfdb2
asafrdt <asafrdt@gmail.com>
2020-03-01 00:43:48 +0530

    Code Refactor And Added Comments - formSubmit.js And Set Default Color

--- form-builder-ui/src/views/formSubmit.js
+++ form-builder-ui/src/views/formSubmit.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import axios from "axios";
 import DynamicForm from "../components/DynamicForm";
-import { Button, Container, Row, Col, Form, InputGroup, FormControl, DropdownButton, Dropdown } from 'react-bootstrap';
+import { Container, Col} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./style.css";
 import { Redirect } from 'react-router-dom';
 
 class FormSubmit extends React.Component {
-
+  // state variables
   state = {
     inputs: [],
     count: 0,
@@ -15,22 +15,18 @@ class FormSubmit extends React.Component {
     validated: false,
     setValidated: false,
     formName: '',
-    formId:''
+    formId: ''
   };
 
-  constructor(props){
+  constructor(props) {
     super(props)
-    console.log(this.props.match.params.formId)
     this.submitForm = this.submitForm.bind(this);
     this.renderRedirect = this.renderRedirect.bind(this);
-    // this.setState({
-    //   formId:this.props.match.params.formId
-    // })
   }
 
-  async componentDidMount() { 
-   await axios.get("/form/"+this.props.match.params.formId).then((response) => {
-      console.log(response);
+  // fetch form
+  componentDidMount() {
+    axios.get("/form/" + this.props.match.params.formId).then((response) => {
       this.setState({
         inputs: response.data.inputs,
         formName: response.data.name,
@@ -42,30 +38,20 @@ class FormSubmit extends React.Component {
     })
   }
 
-  submitForm(input){
-    // console.log('clcik')
-    // console.log(input)
-    // console.log(this.state.inputs)
-    
+  // submit form action
+  submitForm(input) {
     var form = this.state.inputs;
-    // console.log(form)
     var response = {};
 
     Object.keys(form).map((key) => {
-
-      var feildName =form[key].name;
-
-      // console.log(form[key].key)
-
+      var feildName = form[key].name;
       var res = input[form[key].key];
-
-      // console.log(feildName+" - "+res)
-
-      response[feildName] = res ;
+      if(feildName == 'color' && res == null){
+        response[feildName] = '#000000'
+      }else{
+        response[feildName] = res;
+      }
     });
-    // console.log(response)
-    // console.log(response)
-    // console.log(response['name'])
 
     var submissionObj = {
       "form": {
@@ -74,24 +60,23 @@ class FormSubmit extends React.Component {
         "response": response
       }
     }
-
-    // console.log(submissionObj)
     this.saveSubmissionToDb(submissionObj)
   }
 
-  saveSubmissionToDb(payload){
+  // save input data to db
+  saveSubmissionToDb(payload) {
     axios.post('/submit', {
       payload: payload
     }).then((response) => {
-      console.log(response);
       setTimeout(() => {
         this.setState({
           redirect: true
         })
-      }, 3000);
+      }, 2000);
     })
   }
 
+  // redirect action
   renderRedirect() {
     if (this.state.redirect) {
       return <Redirect to='/formList' />
@@ -101,41 +86,19 @@ class FormSubmit extends React.Component {
   render() {
     return (
       <Container className="generatedForm" as={Col} md="5">
-      {this.renderRedirect()}
-      <h1 className="header">{this.state.formName}</h1>
-      {/* <DynamicForm
-        // delete={this.delete}
-        key={this.state.current.id}
-        validated = {this.state.validated} 
-        // onSubmit={this.saveForm}
-        formId="submission-form"
-
-        className="form"
-        title="Registration"
-
-        // defaultValues={this.state.current}
-        inputs={this.state.inputs}
-        submitBtnClick={this.submitForm}
-        submitBtnText = "Submit Form"
-        onSubmit={inputs => {
-          this.submitForm(inputs);
-        }}
-      ></DynamicForm> */}
+        {this.renderRedirect()}
+        <h1 className="header">{this.state.formName}</h1>
         <DynamicForm
-            delete={this.delete}
-            key={this.state.current.id}
-            className="form"
-            // defaultValues={this.state.current}
-            inputs={this.state.inputs}
-            submitBtn= "true"
-            submitBtnText="Submit Form"
-           
-            onSubmit={inputs => {
-              this.submitForm(inputs);
-            }}
-          ></DynamicForm>
-      {/* {this.renderFormAction()} */}
-    </Container>
+          key={this.state.current.id}
+          className="form"
+          inputs={this.state.inputs}
+          submitBtn="true"
+          submitBtnText="Submit Form"
+          onSubmit={inputs => {
+            this.submitForm(inputs);
+          }}
+        ></DynamicForm>
+      </Container>
     )
   }
 }
