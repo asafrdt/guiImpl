32a35598a33bb8b10991c84de26ef172a0dc51a8
asafrdt <asafrdt@gmail.com>
2020-03-12 12:17:33 +0200

    interview exracise

--- form-builder-ui/src/index.js
+++ form-builder-ui/src/index.js
@@ -1,34 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
-import { FormBuilder, FormSubmissions, FormList, FormSubmit } from './views';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Nav } from 'react-bootstrap';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import { FormBuilder, FormSubmissions, FormList, FormSubmit } from "./views";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Nav } from "react-bootstrap";
 
 const routing = (
+  <Router>
+    <Navbar bg="dark" expand="lg" variant="dark">
+      <Navbar.Brand href="/">React-Form-Builder</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Form-Builder</Nav.Link>
+          <Nav.Link href="/formList">Form-List</Nav.Link>
+          <Nav.Link href="/auth/github">Github login</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+    <Route exact path="/" component={FormBuilder} />
+    <Route path="/formList" component={FormList} />
+    <Route path="/submissions/:formId" component={FormSubmissions} />
+    <Route path="/submit/:formId" component={FormSubmit} />
+  </Router>
+);
 
-    <Router>
-        <Navbar bg="dark" expand="lg" variant="dark">
-            <Navbar.Brand href="/">React-Form-Builder</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="/">Form-Builder</Nav.Link>
-                    <Nav.Link href="/formList">Form-List</Nav.Link>
-                    <Nav.Link href="/auth/github">Github login</Nav.Link>
-
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-        <Route exact path="/" component={FormBuilder} />
-        <Route path="/formList" component={FormList} />
-        <Route path="/submissions/:formId" component={FormSubmissions} />
-        <Route path="/submit/:formId" component={FormSubmit}/> 
-    </Router>
-)
-
-ReactDOM.render(routing, document.getElementById('root'));
+ReactDOM.render(routing, document.getElementById("root"));
 
 serviceWorker.unregister();
